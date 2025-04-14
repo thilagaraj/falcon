c4b38bd25174019eef1e9fac0836e66476ec135f
vinothbhc1986 <vinothbhc1986@gmail.com>
2025-04-15 00:45:12 +0530

    alin items

--- src/pages/ReportListPage.jsx
+++ src/pages/ReportListPage.jsx
@@ -17,16 +17,16 @@ const ReportListPage = () => {
       <header className="d-flex flex-column align-items-center bg-success text-white p-3">
         <h1 className="fs-5 text-center">Online Report List</h1>
       </header>
-      <div className="container py-4">
-        <div className="row g-3">
+      <div className="container py-4 d-flex justify-content-center align-items-center" style={{ height: '90vh' }}>
+        <div className="row g-4">
           {items.map((item, index) => (
             <div className="col-6" key={index}>
               <div
-                className={`p-4 text-white text-center rounded ${item.bg}`}
-                style={{ height: '120px', backgroundColor: item.customBg || undefined }}
+                className={`p-4  d-flex flex-column justify-content-center align-items-center text-white rounded ${item.bg}`}
+                style={{ height: '120px', backgroundColor: item.customBg  }}
               >
                 <Icon icon={item.icon} width="48" />
-                <div>{item.label}</div>
+                <div style={{ marginTop: '10px' }}>{item.label}</div>
               </div>
             </div>
           ))}
