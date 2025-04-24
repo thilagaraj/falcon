5e0a2744912434d8e8d524730a52af36ae9dc3cf
vinothbhc1986 <vinothbhc1986@gmail.com>
2025-04-24 18:08:55 +0530

    and click event

--- src/pages/ReportListPage.jsx
+++ src/pages/ReportListPage.jsx
@@ -58,9 +58,9 @@ const dashboardList = [
 const ReportListPage = () => {
   const [dashboard, setDashboard] = useState({});
   const [searchParams] = useSearchParams();
-  const branchCode = searchParams.get("branchCode");
-  const hotelId = searchParams.get("hotelId");
-  const propertyId = searchParams.get("propertyId");
+  const branchCode = searchParams.get("BranchCode");
+  const hotelId = searchParams.get("HotelId");
+  const propertyId = searchParams.get("PropertyId");
   const checkinNo = searchParams.get("CheckinNo");
   const mobileNo = searchParams.get("MobileNo");
   const { showLoading, hideLoading } = useSpinner();
@@ -97,9 +97,7 @@ const ReportListPage = () => {
   };
 
   const handleCardClick = (item) => {
-    // Handle the click event for enabled cards
     console.log('Card clicked:', item);
-    // Add your navigation or action logic here
   };
 
   return (
--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -6,10 +6,34 @@ import { useSpinner } from '../hook/SpinnerContext';
 import $axios from '../utils/axios';
 
 const ReviewPage = () => {
- 
+  const [review, setReview] = useState({});
+  const [searchParams] = useSearchParams();
+  const branchCode = searchParams.get("BranchCode");
+  const hotelId = searchParams.get("HotelId");
+  const propertyId = searchParams.get("PropertyId");
+  const checkinNo = searchParams.get("CheckinNo");
+  const mobileNo = searchParams.get("MobileNo");
+  const { showLoading, hideLoading } = useSpinner();
 
   useEffect(() => {
+    getReview();
   }, []);
+
+  const getReview = async () => {
+    try {
+      showLoading();
+      const response = await $axios.get(
+        `/FalconQRScan/GetReview?BranchCode=${branchCode}&PropertyId=${propertyId}&HotelId=${hotelId}&CheckinNo=${checkinNo}&MobileNo=${mobileNo}`
+      );
+      console.log(response);
+      setDashboard(response);
+      
+    } catch (error) {
+      console.error("Error fetching checkout details:", error);
+    } finally {
+      hideLoading();
+    }
+  };
 
   const getGuestDashboard = async () => {
     try {
