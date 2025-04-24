aed20a967bf40c095d640d76b140f7215ee371d1
vinothbhc1986 <vinothbhc1986@gmail.com>
2025-04-24 15:06:14 +0530

    card style changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ProtectedRoute from "./hook/ProtectedRoute";
 import GuestInformationForm from "./pages/GuestInformationForm";
 import { CheckoutDetails } from "./pages/CheckoutDetails";
 import ReportListPage from "./pages/ReportListPage";
+import ReviewPage from "./pages/ReviewPage";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
             <Route path="/guest-registration" element={<GuestInformationForm />} />
             <Route path="/checkout-details" element={<CheckoutDetails />} />
             <Route path="/report-list" element={<ReportListPage />} />
+            <Route path="/review" element={<ReviewPage />} />
 
             {/* Protected Routes */}
             <Route element={<ProtectedRoute />}>
